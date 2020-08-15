2896b3e8f45f483e203fe8508cafff5796925d72
dasha <68502737+dasha220401@users.noreply.github.com>
2020-08-15 21:12:13 +0300

    hw15: Add distance method to class Point
    Signed-off-by: dasha <68502737+dasha220401@users.noreply.github.com>

--- hw_15/point.py
+++ hw_15/point.py
@@ -1,3 +1,5 @@
+import math
+
 class Point:
     def __init__(self, x, y):
         self.x = x
@@ -5,3 +7,6 @@ class Point:
 
     def print(self):
         print("x = ", str(self.x), "y = ", str(self.y))
+
+    def distance(self, point):
+        return math.sqrt((self.x - point.x) ** 2 + (self.y - point.y) ** 2)
